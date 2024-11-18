d49e4f0d218c7c84b8daac4f3436551f0890efc6
ccutcliffe <ccutcliffe@mail.pima.edu>
2024-11-18 15:44:43 -0700

    Update cis129_lab11_02.py
    Added exception handling.

--- cis129_lab11_02.py
+++ cis129_lab11_02.py
@@ -6,26 +6,37 @@ def main():
     # initialize variables
     total = 0  # sum of grades
     grade_counter = 0 # number of grades entered
-    # begin processing grades
-    with open('grades.txt', mode='r') as grades:\
-        # header
-        print('Grades:')
-        # loop until the file is processed completely
-        for grade in grades:
-            grade_counter += 1
-            total += int(grade)
-            print(int(grade))
-        if grade_counter != 0:
-            # finalize readout
-            print("\nTotal:")
-            print(total)
-            print("\nCount:")
-            print(grade_counter)
-            print("\nAverage:")
-            print(total / grade_counter)
-        # if there are no grades in the file
-        else:
-            print('No grades were entered!')
+    try:
+        # begin processing grades
+        with open('grades.txt', mode='r') as grades:\
+            # header
+            print('Grades:')
+            # loop until the file is processed completely
+            for grade in grades:
+                grade_counter += 1
+                total += int(grade)
+                print(int(grade))
+            if grade_counter != 0:
+                # finalize readout
+                print("\nTotal:")
+                print(total)
+                print("\nCount:")
+                print(grade_counter)
+                print("\nAverage:")
+                print(total / grade_counter)
+            # if there are no grades in the file
+            else:
+                print('No grades were entered!')
+    # exception handling
+    except FileNotFoundError:
+        # grades.txt was missing
+        print('Run the first program first!')
+    except PermissionsError:
+        # program tried to run in a sensitive location
+        print('Permissions error! Try a different location!')
+    except:
+        # some unknown problem happened
+        print('Something went wrong!')
 
 # calls main
 main()
